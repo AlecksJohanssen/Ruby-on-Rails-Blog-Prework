e48830f749bc46defafee848647612dc9bc2169d
AlecksJohanssen <minh.dq1078@sinhvien.hoasen.edu.vn>
2016-05-20 14:30:36 +0700

    Updated with Login Function

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,25 @@
 Rails.application.routes.draw do
   resources :articles
+  resources :articles
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+    root  to: "articles#index"
+    resources :articles do
+  resources :comments
+  end
+    resources :tags
+    resources :logins
+    
+    resources :login_sessions, only: [ :new, :create, :destroy ]
 
+get 'login'  => 'login_sessions#new'
+get 'logout' => 'login_sessions#destroy'
+    
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
