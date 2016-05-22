54e88c07270ef6c82640b25c08176bbb5607738e
AlecksJohanssen <minh.dq1078@sinhvien.hoasen.edu.vn>
2016-05-22 20:53:21 +0700

    Create comments table

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+	before_filter :require_login
+	def create
+  @comment = Comment.new(comment_params)
+  @comment.article_id = params[:article_id]
+  @comment.save
+
+  redirect_to article_path(@comment.artic<le)
+end
+def destroy
+    @article = Article.find(params[:article_id])
+        @comment = Comment.find(params[:id])
+        @comment.destroy
+        respond_to do |format|
+            format.html { redirect_to @article }
+            format.js
+        end
+    end
+
+def comment_params
+  params.require(:comment).permit(:author_name, :body)
+end
+    
+end
