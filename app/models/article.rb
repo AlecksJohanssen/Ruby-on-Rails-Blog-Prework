030052a05753d51bf06c8c8903b0ac620bd451c8
AlecksJohanssen <minh.dq1078@sinhvien.hoasen.edu.vn>
2016-05-25 11:33:29 +0700

    Update Taggings
    #Dependent Destroy

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ActiveRecord::Base
   has_many :taggings
   has_many :tags, through: :taggings
   has_many :comments, dependent: :destroy
+  has_many :taggings, dependent: :destroy
 
   # It returns the articles whose titles contain one or more words that form the query
   def self.search(query)
